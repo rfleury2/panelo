9fd173ef1325f726d3add0ed2f249f951324d0b8
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-07-08 19:49:35 -0500

    Change migration :( It's okay only because I haven't deployed it yet

--- db/migrate/20160708002345_create_contests.rb
+++ db/migrate/20160708002345_create_contests.rb
@@ -2,8 +2,6 @@ class CreateContests < ActiveRecord::Migration
   def change
     create_table :contests do |t|
       t.string :title
-      t.string :short_code
-      t.belongs_to :category
 
       t.timestamps null: false
     end
