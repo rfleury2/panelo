fc2e986ce876674f1030b07f7b2942a87ccc2dc2
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-08-19 22:09:04 -0500

    Add additional seed skeletons

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,3 +74,88 @@ def seed_presidents
   end  
 end
 seed_presidents
+
+contest = Contest.find_or_create_by(title: "Who is the best female tennis player of all time?", category: "sports")
+[
+  "Serena Williams",
+  "Venus Williams",
+  "Chris Every",
+  "Martina Hingis",
+  "Martina Navratilova",
+  "Billy Jean King",
+  "Maria Esther Bueno",
+  "Lindsay Davenport",
+  "Monica Seles",
+  "Steffi Graf",
+  "Justine Henin"
+].each do |name|
+  contest.players.find_or_create_by(name: name)
+end
+
+contest = Contest.find_or_create_by(title: "What team is the best Champions League winner of all time?", category: "sports")
+[
+  "Real Madrid - 2016",
+  "Barcelona - 2015",
+  "Real Madrid - 2014",
+  "Barcelona - 2013",
+  "Chelsea - 2012",
+  "Barcelona - 2011",
+  "Internazionale - 2010",
+  " - 2009",
+  " - 2008",
+  "Milan - 2007",
+  "Liverpool - 2006",
+  " - 2005",
+  " - 2004",
+  "Milan - 2003"
+].each do |name|
+  contest.players.find_or_create_by(name: name)
+end
+
+contest = Contest.find_or_create_by(title: "Who is the best Oscar-winning male leading actor of the modern era?", category: "movies")
+[
+  "Tom Hanks",
+  "Russell Crowe"
+].each do |name|
+  contest.players.find_or_create_by(name: name)
+end
+
+contest = Contest.find_or_create_by(title: "Who is the best Oscar-winning female leading actor of the modern era?", category: "movies")
+[
+  "Tom Hanks",
+  "Russell Crowe"
+].each do |name|
+  contest.players.find_or_create_by(name: name)
+end
+
+contest = Contest.find_or_create_by(title: "What is the best Oscar-winning movie of the modern era?", category: "movies")
+[
+  "Shakespeare in Love",
+  "Crash"
+].each do |name|
+  contest.players.find_or_create_by(name: name)
+end
+
+contest = Contest.find_or_create_by(title: "Who is the greatest rock'n'roll guitarrist of all time?", category: "music")
+[
+  "Eric Clapton",
+  "B.B. King"
+].each do |name|
+  contest.players.find_or_create_by(name: name)
+end
+
+contest = Contest.find_or_create_by(title: "Who is the best X-Men character?", category: "comics")
+[
+  "Wolverine",
+  "Cyclops"
+].each do |name|
+  contest.players.find_or_create_by(name: name)
+end
+
+contest = Contest.find_or_create_by(title: "Who is the best music duo of all time?", category: "music")
+[
+  "Sonny and Cher",
+  "Simon and Garfunkel"
+].each do |name|
+  contest.players.find_or_create_by(name: name)
+end
